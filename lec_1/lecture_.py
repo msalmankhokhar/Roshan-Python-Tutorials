c0a47a5edf81611cb97a11e44c36589110d25f86
msalmankhokhar <kjokhars@gmail.com>
2024-06-24 11:18:51 +0500

    first commit

---
+++ lec_1/lecture_.py
@@ -0,0 +1,21 @@
+name : str = "roshaaan" # string
+
+age : int = 20 # integer
+
+height : float = 5.6 # float / decimal
+
+Im_a_good_boy : bool = True # boolean
+
+# basic / pramitive datatypes
+# derived datatypes
+
+myList = [ "salman", 20, "Roshan" ]
+myTuple = ( "salman", 20, "Roshan" )
+mySet = { "salman", 20, "Roshan" }
+
+# dictionary
+myDict = { "name" : "salman", "age" : 20 }
+
+myDict.pop("age")
+
+print(myDict)
