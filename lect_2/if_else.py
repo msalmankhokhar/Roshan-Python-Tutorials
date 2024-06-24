bb03a96997a2effb65f5c6ed1f36552531ade782
msalmankhokhar <kjokhars@gmail.com>
2024-06-24 12:12:45 +0500

    new file:   lect_2/if_else.py 	new file:   lect_2/lect_operators.py 	new file:   lect_2/typeCasting/typeCasting.py

---
+++ lect_2/if_else.py
@@ -0,0 +1,18 @@
+# If else in python
+
+# age = 18
+
+# if age == 18:
+#     print("Your age is 18")
+# elif age > 18:
+#     print("Your age is above 18")
+# else:
+#     print("All conditions false")
+
+# while loop
+n = 0
+
+myList = ['salman', 'roshan', 'imran', 'usman', 'ali']
+
+for num in range(10):
+    print(num)
