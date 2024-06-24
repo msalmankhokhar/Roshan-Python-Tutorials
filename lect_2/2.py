4851ec9462b75f7e18c2639263399430cc1182d1
msalmankhokhar <kjokhars@gmail.com>
2024-06-24 12:24:10 +0500

    organized code

---
+++ lect_2/2.py
@@ -0,0 +1,34 @@
+# Topic: operators in python
+
+# Arithmetic Operators
+# + - / * %
+
+print(2 + 2) # plus operator
+print(2 - 2) # minus operator
+print(2 / 2) # multiply operator
+print(2 * 2) # multiply operator
+print(2 % 2) # modulus operator (for getting reminder of division)
+
+# Assignment operators
+num = 13
+
+# comparison operators
+# < less than
+# > greater than
+# <= less than or equal to
+# >= greater than or equal to
+
+# Increment operators
+a = 10
+
+a += 5
+print(a)
+
+a -= 5
+print(a)
+
+a /= 5
+print(a)
+
+a *= 5
+print(a)
---
+++ lect_2/3.py
@@ -0,0 +1,6 @@
+# Type casting: converting one datatype to another
+
+num1 = 18
+num2 = "5"
+
+print("5" + "2")
